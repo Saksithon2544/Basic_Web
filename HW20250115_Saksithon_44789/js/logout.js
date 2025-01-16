e1f20e7742f8fc0b5e4eb4a4de84066354b3bc23
Saksithon_2544 <saksithon.mt@technictrang.ac.th>
2025-01-17 00:10:11 +0700

    enhance logout functionality with SweetAlert2 confirmation dialog; update home page layout and styles

--- HW20250115_Saksithon_44789/js/logout.js
+++ HW20250115_Saksithon_44789/js/logout.js
@@ -5,8 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
     logoutButton.addEventListener("click", (e) => {
         e.preventDefault();
 
-        localStorage.removeItem("authToken");
-
-        window.location.href = "../index.html"; 
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You will be logged out!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: 'red',
+            confirmButtonText: 'Yes, logout',
+            cancelButtonText: 'Cancel'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = "./../index.html"; 
+              localStorage.removeItem("authToken");
+            }
+        });
     });
 });
